c1f0f46a41383b854c3943e0ba7cb5de593163f6
Ebrardemir <ebrardemir6106@gmail.com>
2025-04-04 01:16:44 +0300

    ebrar branch: login sayfası yapıldı

---
+++ frontend/src/components/LoginHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../images/kou.png';
+
+function LoginHeader() {
+    return (
+        <div className='login_header'>
+            <img className='logo' src={logo} alt="kou-logo" width={100} height={100} />
+            <h2>ÖĞRENCİ BİLGİ SİSTEMİ / İDARİ PERSONEL</h2>
+        </div>
+    )
+}
+
+export default LoginHeader
