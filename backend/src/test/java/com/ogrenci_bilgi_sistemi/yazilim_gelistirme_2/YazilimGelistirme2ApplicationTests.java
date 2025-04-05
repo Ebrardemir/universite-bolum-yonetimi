246d20d99654d0af04833b5618ab3091a25d9ed6
miran5501 <esermiran1@gmail.com>
2025-04-05 23:35:11 +0300

    İlk backend projesi taslağı

---
+++ backend/src/test/java/com/ogrenci_bilgi_sistemi/yazilim_gelistirme_2/YazilimGelistirme2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogrenci_bilgi_sistemi.yazilim_gelistirme_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YazilimGelistirme2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
